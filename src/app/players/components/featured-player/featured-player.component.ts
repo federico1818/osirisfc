860c103c79a095a81ddf549c16c29cb4d39e1856
Federico González <federico1818@gmail.com>
2023-12-23 23:19:06 -0300

    create files featured player

---
+++ src/app/players/components/featured-player/featured-player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-featured-player',
+    templateUrl: './featured-player.component.html',
+    styleUrls: ['./featured-player.component.scss']
+})
+
+export class FeaturedPlayerComponent {
+
+}
--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { PlayersRoutingModule } from './players-routing.module';
-import { PlayersComponent } from './pages/players/players.component'
+import { PlayersComponent } from './pages/players/players.component';
+import { FeaturedPlayerComponent } from './components/featured-player/featured-player.component'
 
 @NgModule({
     declarations: [
-        PlayersComponent
+        PlayersComponent,
+        FeaturedPlayerComponent
     ],
     imports: [
         CommonModule, 
